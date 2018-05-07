beeb6ca3de5d5beecd8f9c9488287a702ffa4307
Steven <tassteven.sc@gmail.com>
2018-05-07 16:17:56 -0700

    corrected syntax

--- createSeed.js
+++ createSeed.js
@@ -1,41 +1,41 @@
 const fs = require('fs');
 const faker = require('faker');
 
-let entryNum = 10000000;
+const entryNum = 10000000;
 
-let createEntry = (i) => {
-  let reviewsArr = [];
-  for (var j = 0; j <= Math.floor(Math.random()*10); j++) {
+const createEntry = (i) => {
+  const reviewsArr = [];
+  for (let j = 0; j <= Math.floor(Math.random() * 10); j++) {
     reviewsArr.push({
-      "author_name": faker.name.findName(),
-      "profile_photo_url": faker.image.avatar(),
-      "rating": Math.floor(Math.random()*5),
-      "relative_time_description": faker.date.past(),
-      "text": faker.lorem.sentence()
-    })
+      author_name: faker.name.findName(),
+      profile_photo_url: faker.image.avatar(),
+      rating: Math.floor(Math.random() * 5),
+      relative_time_description: faker.date.past(),
+      text: faker.lorem.sentence(),
+    });
   }
 
   const obj = {
-    place_id: "" + i,
+    place_id: `${i}`,
     name: faker.company.companyName(),
     reviews: reviewsArr,
-    rating: Math.floor(Math.random()*5),
-    price_level: Math.floor(Math.random()*4),
+    rating: Math.floor(Math.random() * 5),
+    price_level: Math.floor(Math.random() * 4),
     neighborhood: faker.address.county(),
     city: faker.address.city(),
     street: faker.address.streetName(),
   };
 
   return obj;
-}
+};
 
-let generateJSON = () => {
-  var options = {
-    autoClose: true
+const generateJSON = () => {
+  const options = {
+    autoClose: true,
   };
-  let writeStream = fs.createWriteStream('millionData.json', options);
+  const writeStream = fs.createWriteStream('millionData.json', options);
   let i = -1;
-  const write = function() {
+  const write = () => {
     let ok = true;
     do {
       i++;
@@ -50,49 +50,10 @@ let generateJSON = () => {
     }
   };
   write();
-  // let options = {
-  //   autoClose: true
-  // };
-
-  // let writeStream = fs.createWriteStream('millionData.json', options);
-  // let i = -1;
-  // let write = function() {
-  //   var ok = true;
-  //   do {
-  //     i++;
-  //     if (i === 0) {
-  //       writeStream.write('[' + JSON.stringify(createEntry(i))+ ',');
-  //     } else if (i === entryNum) {
-  //       writeStream.write(JSON.stringify(createEntry(i)) + ',');
-  //     } else {
-  //       ok = writeStream.write(JSON.stringify(createEntry(i))+ ',');
-  //     }
-  //   } while (i <= entryNum && ok);
-  //   if (i <= entryNum) {
-  //     writeStream.once('drain', write);
-  //   }
-  // };
-  // write();
-}
-    // if (i === counter - 1) {
-    //   fs.appendFileSync('./millionData.json', JSON.stringify(obj)+']', function (err) {
-    //   if (err) throw err;
-    //   console.log('Saved!', obj);
-    //   });
-    // } else {
-    //   fs.appendFileSync('./millionData.json', JSON.stringify(obj)+',', function (err) {
-    //   if (err) throw err;
-    //   console.log('Saved!', obj);
-    //   });
-    // }
-  
+};
 generateJSON();
 
 // truncate -s -1 millionData.json
 // echo ] >> millionData.json
 // mongoimport --jsonArray -d apateez-reviews -c stores --file millionData.json --numInsertionWorkers 8
-
-
-
-
 
